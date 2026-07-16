8fdc4cefde2103589708c18aa0261b0c01d9c46c
Jessica Holt <jholt08021990@gmail.com>
2026-07-15 21:03:56 -0500

    prepped for deployment

--- app/utils/util.py
+++ app/utils/util.py
@@ -3,8 +3,9 @@ from jose import jwt
 import jose
 from functools import wraps
 from flask import request, jsonify
+import os
 
-SECRET_KEY = "super secret key"
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'super super secret'
 
 def encode_token(customer_id):
     payload = {
--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import os
+
+
 class DevelopmentConfig:
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:poop2025@localhost/mechanic_shop_db'
     DEBUG = True
@@ -9,4 +12,6 @@ class TestingConfig:
     RATELIMIT_STORAGE_URI = 'memory://'
 
 class ProductionConfig:
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    CACHE_TYPE = 'SimpleCache'
+    
