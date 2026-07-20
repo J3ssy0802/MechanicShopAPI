5cc8529ade5cadb9f259f5cdea5aac6d179f6b0d
Jessica Holt <jholt08021990@gmail.com>
2026-07-20 18:52:32 -0500

    changed flask_app

--- flask_app.py
+++ flask_app.py
@@ -8,7 +8,3 @@ app = create_app('ProductionConfig')
 
 with app.app_context():
     db.create_all()
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
